469d0f4f9037a4b60fcefde610cc9c32a04e55f3
hungrychef-bytescode <hungrychefbytescode@gmail.com>
2026-03-08 10:12:06 +0800

    feat: added game navigation routes and initial pages

---
+++ ayos_game/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import "start_screen.dart";
+import "level_screen.dart";
+import "game_screen.dart";
+
+void main() => runApp(const Home())
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/startScreen": (context) => const StartScreen(),
+        "/levelScreen": (context) => const LevelScreen(),
+        "/gameScreen": (context) => const GameScreen(),
+      }
+    );
+  }
+}
